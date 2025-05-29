c4f217a4021a646ff635287f29086dce89196220
Michael Manley <manleym362@gmail.com>
2025-05-29 16:08:17 +0200

    Migration + URL's + gitignroe update

--- backend/backend/urls.py
+++ backend/backend/urls.py
@@ -4,9 +4,12 @@ from api.views import CreateUserView
 from rest_framework_simplejwt.views import TokenObtainPairView, TokenRefreshView
 
 urlpatterns = [
+    # Authentication
     path('admin/', admin.site.urls),
     path('api/user/register/', CreateUserView.as_view(), name='register_user'),
     path('api/token/', TokenObtainPairView.as_view(), name='get_token'),
     path('api/token/refresh/', TokenRefreshView.as_view(), name='refresh'),
-    path('api-auth/', include('rest_framework.urls'))
+    path('api-auth/', include('rest_framework.urls')),
+    # API resources
+    path('api/', include('api.urls')) # Include the API URLs for notes and other resources that aren't listed above
 ]
